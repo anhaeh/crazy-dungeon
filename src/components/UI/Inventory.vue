042e676859a3a43886155c1552bdcb65392f0d4b
Andres Haehnel <anhaeh@gmail.com>
2019-12-31 01:53:10 -0300

    inventary logic selected

--- src/components/UI/Inventory.vue
+++ src/components/UI/Inventory.vue
@@ -1,22 +1,14 @@
 <template>
   <div v-if="getInventory.show" class="inventory">
-    <div class="inventory__slot --selected"></div> 
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div>
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div> 
-    <div class="inventory__slot"></div> 
-<!--     <div  class="item-preview"
-          v-for="(item, index) in getInventory.items"
-         :key="'item' + index">
-      <img :src="image(item)">
-    </div> -->
+    <div v-for="index in getSlots()"
+         :key="'slot' + index"
+         :id="'slot' + index"
+         class="inventory__slot"
+         :class="{'--selected': index === selected}"
+         @click="existItem(index) ? selected = index : ''"
+    >
+      <img v-if="existItem(index)" :src="image(index)">
+    </div>
   </div>
 </template>
 
@@ -28,11 +20,27 @@ export default {
   computed: {
     ...mapGetters([
         'getInventory'
-    ]),
+    ])
+  },
+  data () {
+    return {
+      selected: null
+    }
   },
   methods: {
-    image: function (item) {
+    image: function (index) {
+      let item = this.getInventory.items[index]
       return require('@/assets/items/' + item.image)
+    },
+    existItem: function (index) {
+      return this.getInventory.items[index]
+    },
+    getSlots: function () {
+      let list = [];
+      for (let i = 0; i <= 5; i++) {
+        list.push(i)
+      }
+      return list
     }
   }
 };
@@ -60,6 +68,10 @@ export default {
   background-image: url("../../assets/ui/slot.jpg")
   &.--selected
     box-shadow: inset 0 0 0 var(--pixel-unit) yellow
+  img
+    width: 100%
+    height: 100%
+    object-fit: contain
   // .item-preview
   //   width: calc(1.5 * var(--tile-cell))
   //   height: calc(1.5 * var(--tile-cell))
@@ -68,7 +80,5 @@ export default {
   //   image-rendering: pixelated
   //   background-size: contain
   //   box-sizing: border-box
-  //   img
-  //     width: 100%
-  //     height: 100%
+
 </style>
