74ca3fbcf90e0fc7db5cbc7834d9160ceb2a0b12
Andres Haehnel <ahaehnel@kiusys.com>
2020-03-02 11:13:15 -0300

    reload app when there are changes

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -20,12 +20,8 @@ if (process.env.NODE_ENV === 'production') {
       console.log('New content is downloading.')
     },
     updated () {
-      console.log('New content is available; please refresh.')
-      caches.keys().then(cacheNames => {
-        cacheNames.forEach(cacheName => {
-          caches.delete(cacheName)
-        })
-      })
+      console.log('New content is available.')
+      location.reload(true)
     },
     offline () {
       console.log('No internet connection found. App is running in offline mode.')
