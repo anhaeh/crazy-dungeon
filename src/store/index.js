6f28025225a874805ac9518e772f76eef97ef369
Andres Haehnel <ahaehnel@kiusys.com>
2020-08-20 19:49:05 -0300

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -132,7 +132,7 @@ const store = new Vuex.Store({
   mutations: {
     setClassSelected(state, classSelected) {
       state.classSelected = classSelected
-      state.dungeon = 10
+      state.dungeon = 0
     },
     setMonsterSelected(state, monsterSelected) {
       state.monsterSelected = monsterSelected
