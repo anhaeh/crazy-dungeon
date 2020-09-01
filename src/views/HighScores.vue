83c6bc794d0e955e49465eafd630ce7c27a703fe
Andres Haehnel <ahaehnel@kiusys.com>
2020-09-01 13:09:30 -0300

    fixes pyro class

--- src/views/HighScores.vue
+++ src/views/HighScores.vue
@@ -79,6 +79,8 @@ td
     background-image: url("../assets/heroes/wizard.gif")
   &.--paladin
     background-image: url("../assets/heroes/paladin.gif")
+  &.--pyromancer
+    background-image: url("../assets/heroes/pyromancer.gif")
 .table-score
   width: 85%
   height: 60%
